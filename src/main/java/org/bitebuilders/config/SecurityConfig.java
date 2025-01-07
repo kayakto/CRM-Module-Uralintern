c3437287889a9d4a33c1d853f4c1388c00203d45
kayakto <alexeev.egor.list.2004@gmail.com>
2025-01-07 16:26:13 +0500

    add CORS for security config

--- src/main/java/org/bitebuilders/config/SecurityConfig.java
+++ src/main/java/org/bitebuilders/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.bitebuilders.config;
 import org.bitebuilders.component.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -36,6 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
 //                        .csrfTokenRepository(csrfTokenRepository())
 //                        .ignoringRequestMatchers(
